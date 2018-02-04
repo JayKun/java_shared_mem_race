2e0fa600deac13b1fb58df76db3eda8744ed7068
JayKun <junkaiong11@gmail.com>
2018-02-04 15:25:56 -0800

    get and set

---
+++ GetNSet.java
@@ -0,0 +1,16 @@
+import  java.util.concurrent.atomic;
+
+public class GetNSet implements State{
+	private AtomicIntegerArray m_arry;
+	public byte[] current(){
+
+	}
+	public GetNSet(){
+		
+	}
+	
+	public int size(){return m_array.length();}
+	public boolean swap(int i, int j){
+		
+	}
+}
